a14562294a4ba795019927235d930903f3f7b002
DESKTOP-D9V8CFP\BPS_LT2 <bps.s-umeda@bpsc.co.jp>
2020-12-17 12:28:46 +0900

    no message

--- BPS_NOTE/CAP_Ctrl003.ino
+++ BPS_NOTE/CAP_Ctrl003.ino
@@ -14,18 +14,19 @@
 #define PORT_SENSOR_PE 2
 #define PORT_SENSOR_W 3
 
-#define PORT_SENSOR_PASS_2 6
-#define PORT_SENSOR_PASS_3 7
+#define PORT_SENSOR_PASS_2 4
+#define PORT_SENSOR_PASS_3 5
 
 #define INT_NUM_AREA2 0
 #define INT_NUM_AREA3 1
 
-#define PORT_AIR_2 10
-#define PORT_AIR_3 11
+#define PORT_AIR_PE 6
+#define PORT_AIR_W 7
 
-bool blPE = false;
-bool blW = false;
+#define CNT_END_NUM 5000
+#define CNT_FLG_CANCEL 62000
 
+#define PORT_DEBUG 12
 
 #include "Timer.h"
 
@@ -35,7 +36,7 @@ bool blW = false;
 void setup() {
     InitPort();//ポートのイニシャライズ
 
-    FlashLED(10, 50, LED_OFF);
+    FlashLED(3, 50, LED_OFF);
 
     attachInterrupt(INT_NUM_AREA2, FlgSetAir2, FALLING);
     attachInterrupt(INT_NUM_AREA3, FlgSetAir3, FALLING);
@@ -45,24 +46,122 @@ void setup() {
 }
 
 //*********************************************************************************
-void loop() {
-    if (blPE && PORT_SENSOR_PE == HIGH)
+long CntAirPE = 0;
+long CntAirW = 0;
+
+long CntFlgPE = 0;
+long CntFlgW = 0;
+
+//bool BlCntRunPE = false;
+//bool BlCntRunW = false;
+
+bool FlgPE = false;
+bool FlgW = false;
+
+void loop() 
+{
+    CtrlPE(FlgPE);
+    CtrlW(FlgW);
+
+    if(FlgPE || FlgW)
     {
-        
+        digitalWrite(PORT_DEBUG, LOW);
+        digitalWrite(PORT_LED, LED_ON);
     }
-    
-
-    if (blW && PORT_SENSOR_W == HIGH)
+    else
     {
-
+        digitalWrite(PORT_DEBUG, HIGH);
+        digitalWrite(PORT_LED, LED_OFF);
+    }
+}
+//*********************************************************************************
+//*********************************************************************************
+void CtrlPE(bool& flg)
+{
+    if(flg)
+    {
+        //CAP通過が終わったらPE命令発信
+        if(digitalRead(PORT_SENSOR_PASS_2) == HIGH)
+        {
+            digitalWrite(PORT_AIR_PE, AIR_ON);
+            flg *= false;
+            Serial.println("-AirPE Start-");
+            CntFlgPE = 0;
+        }
+        else
+        {
+            CntFlgPE++;
+            if(CntFlgPE > CNT_FLG_CANCEL)
+            {
+                flg *= false;
+                CntFlgPE = 0;
+            }
+        }
+    }
+    else
+    {
+        if(digitalRead(PORT_AIR_PE) == AIR_ON)
+        {
+            CntAirPE++;
+            if(CntAirPE >= CNT_END_NUM)
+            {
+                //digitalWrite(PORT_LED, LED_OFF);
+                digitalWrite(PORT_AIR_PE, AIR_OFF);
+                CntAirPE = 0;
+                Serial.println("-AirPE Stop-");
+            }
+        }
     }
 }
 
+//*********************************************************************************
+void CtrlW(bool& flg)
+{
+    if(flg)
+    {
+        //CAP通過が終わったらPE命令発信
+        if(digitalRead(PORT_SENSOR_PASS_3) == HIGH)
+        {
+            digitalWrite(PORT_AIR_W, AIR_ON);
+            flg *= false;
+            Serial.println("-AirW Start-");
+            CntFlgW = 0;
+        }
+        else
+        {
+            CntFlgW++;
+            if(CntFlgW > CNT_FLG_CANCEL)
+            {
+                flg *= false;
+                CntFlgW = 0;
+            }
+        }
+    }
+    else
+    {
+        if(digitalRead(PORT_AIR_W) == AIR_ON)
+        {
+            CntAirW++;
+            if(CntAirW >= CNT_END_NUM)
+            {
+                //digitalWrite(PORT_LED, LED_OFF);
+                digitalWrite(PORT_AIR_W, AIR_OFF);
+                CntAirW = 0;
+                Serial.println("-AirW Stop-");
+            }
+        }
+    }
+}
 //*********************************************************************************
 //*********************************************************************************
 //*********************************************************************************
 void InitPort()
 {
+    //----------------------------
+    pinMode(PORT_DEBUG, OUTPUT);
+    digitalWrite(PORT_DEBUG, HIGH);
+    //----------------------------
+
 
     pinMode(PORT_SENSOR_PE, INPUT_PULLUP);
     pinMode(PORT_SENSOR_W, INPUT_PULLUP);
@@ -70,12 +169,12 @@ void InitPort()
     pinMode(PORT_SENSOR_PASS_2, INPUT_PULLUP);
     pinMode(PORT_SENSOR_PASS_3, INPUT_PULLUP);
 
-    pinMode(PORT_AIR_2, OUTPUT);
-    pinMode(PORT_AIR_3, OUTPUT);
+    pinMode(PORT_AIR_PE, OUTPUT);
+    pinMode(PORT_AIR_W, OUTPUT);
     pinMode(PORT_LED, OUTPUT);
 
-    digitalWrite(PORT_AIR_2, AIR_OFF);
-    digitalWrite(PORT_AIR_3, AIR_OFF);
+    digitalWrite(PORT_AIR_PE, AIR_OFF);
+    digitalWrite(PORT_AIR_W, AIR_OFF);
     digitalWrite(PORT_LED, LED_OFF);
 
     Serial.println("-----START-----");
@@ -87,9 +186,9 @@ void FlashLED(int loopNum, int delayTime, bool blEnd)
     for (int i = 0; i < loopNum; i++)
     {
         digitalWrite(PORT_LED, LED_ON);
-        delay(delayTime);
+        delay(delayTime/10);
         digitalWrite(PORT_LED, LED_OFF);
-        delay(delayTime);
+        delay(delayTime*10);
     }
     digitalWrite(PORT_LED, blEnd);
 
@@ -98,8 +197,9 @@ void FlashLED(int loopNum, int delayTime, bool blEnd)
 //*********************************************************************************
 void FlgSetAir2()
 {
-    blPE = true;
-    digitalWrite(PORT_LED, !digitalRead(PORT_LED));
+    FlgPE = true;
+    digitalWrite(PORT_DEBUG, LOW);
+    digitalWrite(PORT_LED, LED_ON);
     Serial.println("Air2 Flg Set");
 }
 
@@ -107,18 +207,11 @@ void FlgSetAir2()
 //*********************************************************************************
 void FlgSetAir3()
 {
-    blW = true;
+    FlgW = true;
     digitalWrite(PORT_LED, !digitalRead(PORT_LED));
+    digitalWrite(PORT_DEBUG, LOW);
+    digitalWrite(PORT_LED, LED_ON);
     Serial.println("Air3 Flg Set");
 }
 
-
-//*********************************************************************************
-void ExeAir(int portNum)
-{
-    digitalWrite(portNum, LOW);
-
-    if (portNum == PORT_AIR_2);
-    if (portNum == PORT_AIR_3);
-}
 
