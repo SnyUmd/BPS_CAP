ed4badc4d718ce7f454b7e3e79a80f209630f47d
sny-umd <sny.umd@gmail.com>
2020-12-27 02:02:21 +0900

    Revert "Revert "コード整理""
    This reverts commit c9349d9d4074911fd6129b90079a3e5a84214b4a.

--- UMD_NOTE/Init.h
+++ UMD_NOTE/Init.h
@@ -5,29 +5,15 @@ static bool FlgPE = false;
 static bool FlgW = false;
 static int DebugCntAAA = 0;
 
-struct _structCNT
+extern long AryStsPE[5];
+extern long AryStsW[5];
+enum _EnmArySts
 {
-    long AirPE = 0;
-    long AirW = 0;
-
-    long FlgPE = 0;
-    long FlgW = 0;
-
-    long WaitPassPE = 0;
-    long WaitPassW = 0;
-
-    long testAAA = 0;
+    cntAir = 0,
+    cntFlg,
+    cntWaitPass,
+    timeStart,
+    timeSpeed
 };
-static struct _structCNT St_Cnt;
-
-//通過時間カウント用
-struct _Time
-{
-    long T_StartPE = 0;
-    long T_StartW = 0;
-    long T_SpeedPE = 0;
-    long T_SpeedW = 0;
-};
-static struct _Time St_Time;
 
 void InitPort();
